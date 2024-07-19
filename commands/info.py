5a51adbe447f58c45be3d76989e945ac1b78989a
Cameron Cooper <cameron.cooper@loyalhealth.com>
2024-07-18 20:59:26 -0400

    improve upon buddy info command

--- commands/info.py
+++ commands/info.py
@@ -1,25 +1,43 @@
+from config.config_manager import ConfigManager
+from test.test___future__ import features
+
+
 def display_info():
     """
     Displays information about the Buddy CLI.
     """
+
+    config = ConfigManager()
+    features = config.get_features()
     
-    info_text = """
-    Buddy CLI - Command Line Utility for Generative AI
+    current_model = config.get_current_model()
+    enabled_features = len(features) > 0 and ", ".join(features) or "None"
+    
+    info_text = f"""
+    Buddy CLI - Command Line Utility powered by Generative AI
+
+    Current Model: {current_model}
+    Enabled Features: {enabled_features}
 
     Usage:
-        buddy <command> [options]
+        buddy <task> - Execute a task in the shell without supervision
+        buddy <command> [options] - Execute a Buddy command
 
     Commands:
-        info          Display this information
-        help          Work through a task collaboratively
-        carefully     Execute commands with confirmation.
-        use <feature> [value]  Configure Buddy to use a specific model or feature.
+        info                                Display this information
+        help        <task>                  Work through a task collaboratively
+        carefully   <task>                  Execute commands with confirmation on non-read operations
+        explain     <command>               Provide an educational explanation of a command
+        use         <feature> [arguments]   Configure Buddy to use a specific model or feature.
 
     Features:
         gpt4o <apiKey>    Configure Buddy to use OpenAI GPT-4 and save the specified API key.
-        chrome            Configure Buddy to use headless Chrome browser (Selenium) for web research.
 
     Examples:
+        buddy what's my local IP address
+        buddy carefully remove kubernetes
+        buddy help me set up a Minecraft server
+        buddy explain source ~/.bashrc
         buddy use gpt4o YOUR_API_KEY
         buddy use chrome
     """
