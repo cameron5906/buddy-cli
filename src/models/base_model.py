939bff52881c1c942b36243600dba6c25013717c
Cameron Cooper <cameron.cooper@loyalhealth.com>
2024-07-27 22:30:25 -0400

    Thin out model implementations, move everything into flows

--- src/models/base_model.py
+++ src/models/base_model.py
@@ -1,4 +1,3 @@
-import initialize_abilities
 import sys
 from abilities import get_ability
 from config.secure_store import SecureStore
@@ -13,6 +12,7 @@ class BaseModel:
     api_key = None
     ability_actions = []
     ability_prompts = {}
+    require_supervision = False
     is_executing_ability = False
     
     def __init__(self):
@@ -52,63 +52,8 @@ class BaseModel:
                     self.ability_actions.append(action)
             else:
                 print_fancy(f"No actions found for {ability_name} ability.", bold=True, color="red")
-
-    def execute_unsupervised(self, query):
-        """
-        Method for performing unsupervised tasks that don't require any user interaction.
-        
-        Args:
-            query (str): The task to be performed
-            
-        Raises:
-            NotImplementedError: Subclasses should implement this method
-        """
-        
-        raise NotImplementedError("Subclasses should implement this method")
     
-    def execute_carefully(self, query):
-        """
-        Method for performing supervised tasks that require user confirmation before execution of non-read commands.
-        
-        Args:
-            query (str): The task to be performed
-            
-        Raises:
-            NotImplementedError: Subclasses should implement this method
-        """
-        
-        raise NotImplementedError("Subclasses should implement this method")
-    
-    def execute_educational(self, query):
-        """
-        Method for walking a user through a task step-by-step while being informative and educational.
-        
-        Args:
-            query (str): The task for which help is required
-            
-        Raises:
-            NotImplementedError: Subclasses should implement this method
-        """
-        
-        raise NotImplementedError("Subclasses should implement this method")
-    
-    def explain(self, command_string):
-        """
-        Generates a detailed explanation of a command for educational purposes.
-        
-        Args:
-            command_string (str): The command to explain
-            
-        Returns:
-            str: The explanation of the command
-            
-        Raises:
-            NotImplementedError: Subclasses should implement this method
-        """
-        
-        raise NotImplementedError("Subclasses should implement this method")
-    
-    def expand_system_prompt(self, base_prompt):
+    def enhance_system_prompt(self, base_prompt):
         """
         Expands the provided system with extra ability information, if available.
         """
@@ -122,7 +67,7 @@ class BaseModel:
         
         return base_prompt
     
-    def run_inference(self, messages, tools=None, temperature=0.0):
+    def run_inference(self, messages, tools=None, temperature=0.0, require_tool_usage=False):
         """
         Runs inference on the messages and tools provided.
         
@@ -130,6 +75,7 @@ class BaseModel:
             messages (list): The messages to process
             tools (list): The tools to process
             temperature (float): The temperature to use for the model
+            require_tool_usage (bool): Whether to require tool usage
             
         Returns:
             tuple: A tuple containing a boolean indicating if the process is finished, a boolean indicating if the process failed, and a list of messages to return
@@ -137,7 +83,7 @@ class BaseModel:
         
         raise NotImplementedError("Subclasses should implement this method")
     
-    def make_tool(self, tool_name, description, args={}, required=[], json_parameter_schema=None):
+    def make_tool(self, tool_name, description, args=None, required=None, json_parameter_schema=None):
         """
         Creates the structure for describing tools to the model.
         
@@ -168,13 +114,13 @@ class BaseModel:
         
         raise NotImplementedError("Subclasses should implement this method")
     
-    def get_tool_call(self, tool_name, response):
+    def get_tool_call(self, tool_name, completion_obj):
         """
         Gets the calls for a tool by name.
         
         Args:
             tool_name (str): The name of the tool
-            response (dict): The response from the model
+            completion_obj (dict): The model completion object to check for the tool call
             
         Returns:
             list: The call id, arguments, and call dictionary
@@ -195,6 +141,29 @@ class BaseModel:
         
         raise NotImplementedError("Subclasses should implement this method")
 
+    def make_ability_action_tools(self):
+        """
+        Method to create tools for the ability actions that are available to the model
+        """
+        
+        tools = []
+        
+        for action in self.ability_actions:
+            # Ability_actions store the name as {ability_name}_{action_name}, so we need to substring to get the actual action name
+            ability_name = action["name"].split("_")[0]
+            action_name = action["name"][len(ability_name) + 1:]
+            
+            tools.append(
+                self.make_tool(
+                    action_name,
+                    action["description"],
+                    action["argument_schema"],
+                    action["required_arguments"]
+                )
+            )
+            
+        return tools
+
     def handle_internal_tools(self, response, require_mutation_approval=False):
         """
         Handles built-in tools for regular Buddy flows.
@@ -211,7 +180,9 @@ class BaseModel:
             list: A list of messages to add to the chat context
         """
         
-        if response.choices[0].finish_reason != "tool_calls":
+        message = response.choices[0].message
+        
+        if message.tool_calls is None or len(message.tool_calls) == 0:
             return False, None, []
         
         is_finished = False
@@ -219,7 +190,7 @@ class BaseModel:
         returned_messages = []
         
         # Handle provide_plan
-        provide_plan_id, provide_plan_args, provide_plan_call = self.get_tool_call("provide_plan", response)
+        provide_plan_id, provide_plan_args, provide_plan_call = self.get_tool_call("provide_plan", message)
         if provide_plan_id is not None:
             format_markdown_for_terminal(provide_plan_args['plan'])
             
@@ -237,12 +208,12 @@ class BaseModel:
             returned_messages.append(self.make_tool_result(provide_plan_call, content))
 
         # Handle provide_explanation
-        provide_explanation_id, provide_explanation_args, provide_explanation_call = self.get_tool_call("provide_explanation", response)
+        provide_explanation_id, provide_explanation_args, provide_explanation_call = self.get_tool_call("provide_explanation", message)
         if provide_explanation_id is not None:
             format_markdown_for_terminal(f"### {provide_explanation_args['title']}\n{provide_explanation_args['explanation']}")
             returned_messages.append(self.make_tool_result(provide_explanation_call, "Success"))
 
-        provide_resolution_id, provide_resolution_args, provide_resolution_call = self.get_tool_call("provide_resolution", response)
+        provide_resolution_id, provide_resolution_args, provide_resolution_call = self.get_tool_call("provide_resolution", message)
         if provide_resolution_id is not None:
             format_markdown_for_terminal(provide_resolution_args['resolution'])
             
@@ -253,7 +224,7 @@ class BaseModel:
             returned_messages.append(self.make_tool_result(provide_resolution_call, "Success"))
            
         # Handle provide_command 
-        provide_command_id, provide_command_args, provide_command_call = self.get_tool_call("provide_command", response)
+        provide_command_id, provide_command_args, provide_command_call = self.get_tool_call("provide_command", message)
         if provide_command_id is not None:
             print_fancy(f"Proposed command: {provide_command_args['command']}", bold=True, bg="yellow", color="black")
             print_fancy("Do you approve? (y/n)", italic=True, color="blue")
@@ -270,7 +241,7 @@ class BaseModel:
             returned_messages.append(self.make_tool_result(provide_command_call, "Success"))
             
         # Handle execute_command
-        execute_command_id, execute_command_args, execute_command_call = self.get_tool_call("execute_command", response)
+        execute_command_id, execute_command_args, execute_command_call = self.get_tool_call("execute_command", message)
         if execute_command_id is not None:
             if require_mutation_approval and "dangerous" in execute_command_args and execute_command_args["dangerous"]:
                 is_approved = False
@@ -307,14 +278,18 @@ class BaseModel:
                 returned_messages.append(self.make_tool_result(execute_command_call, f"Execution complete\n\n### Stdout Summary\n{stdout}\n\n### Stderr Summary\n{stderr}"))
                
         # Handle end_process 
-        end_process_id, end_process_args, end_process_call = self.get_tool_call("end_process", response)
+        end_process_id, end_process_args, end_process_call = self.get_tool_call("end_process", message)
         if end_process_id is not None:
             is_finished = True
             if not end_process_args['success']:
                 is_failure = True
                 
-            if end_process_args['summary']:
+            if "summary" in end_process_args and "details" in end_process_args:
+                format_markdown_for_terminal(f"### Summary\n{end_process_args['summary']}\n\n### Details\n{end_process_args['details']}")
+            elif "summary" in end_process_args:
                 format_markdown_for_terminal(end_process_args['summary'])
+            elif "details" in end_process_args:
+                format_markdown_for_terminal(end_process_args['details'])
             
             returned_messages.append(self.make_tool_result(end_process_call, "Success"))
             
@@ -323,7 +298,7 @@ class BaseModel:
             ability_name = ability_action_name.split("_")[0]
             action_tool_name = ability_action_name[len(ability_name) + 1:]
             
-            ability_action_call_id, ability_action_args, ability_action_call = self.get_tool_call(action_tool_name, response)
+            ability_action_call_id, ability_action_args, ability_action_call = self.get_tool_call(action_tool_name, message)
             if ability_action_call_id is not None:
                 ability = get_ability(ability_name)
                 
@@ -345,7 +320,7 @@ class BaseModel:
                 returned_messages.append(self.make_tool_result(ability_action_call, tool_output if tool_output else "Success"))
 
         # Check for unhandled tools and generate error responses
-        unhandled_calls = self.get_unhandled_tool_calls(response.choices[0].message, returned_messages)
+        unhandled_calls = self.get_unhandled_tool_calls(message, returned_messages)
         for unhandled_call in unhandled_calls:
             returned_messages.append({
                 "role": "tool",
